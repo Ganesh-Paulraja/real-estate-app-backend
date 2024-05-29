edba929ef6f204314e3dd642ce0352370a889c7e
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-05-29 19:08:31 +0530

    update index

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from '../models/user.model.js'
-
+import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({username, email, password});
   await newUser.save();
-  // console.log(newUser);
+  const hashedPassword = bcryptjs.hashSync(password, 10)  //<--this nomber going to merge with bcrypt
+  const newUser = new User({username, email, hashedPassword});
   res.status(201).json("user created successfully")
 }
 
--- index.js
+++ index.js
@@ -2,15 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config();
-<<<<<<< HEAD
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
-mongoose.connect(process.env.MONGO)
-.then(() => {
-=======
 
 mongoose.connect(process.env.MONGO).then(() => {
->>>>>>> ac4692313567ede21969d7db19367f2b8527ca18
   console.log('Connected to MongoDB!')
 }).catch((err) => {
   console.log(err);
@@ -21,13 +16,10 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
-<<<<<<< HEAD
 })
 
 
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-=======
-})
->>>>>>> ac4692313567ede21969d7db19367f2b8527ca18
+
