b94b880540c2ab38af32c4cb851a1bb962ac69b1
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-05-29 19:16:29 +0530

    create signup api route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,10 +3,14 @@ import bcryptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
-  await newUser.save();
   const hashedPassword = bcryptjs.hashSync(password, 10)  //<--this nomber going to merge with bcrypt
-  const newUser = new User({username, email, hashedPassword});
-  res.status(201).json("user created successfully")
+  const newUser = new User({username, email, password: hashedPassword});
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully")
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 }
 
 // console.log(new User())
