e7cbfb1649d73271c06dfbe25793b8dd3cf2fdc6
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-06-27 20:11:50 +0530

    user update changes

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -9,24 +9,32 @@ export const test = (req, res) => {
 }
 
 export const updateUserInfo = async (req, res, next) => {
-  if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account'))
-  try {
-    if(req.body.password) {
-      req.body.password = bcryptjs.hashSync(req.body.password, 10)
-    }
-    const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-      // use this method to avoid admin permision from testing
-      $set: {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        avatar: req.body.avatar,
+  if (req.user && req.user.id == req.params.id) {
+    try {
+      if (req.body.password) {
+        req.body.password = bcryptjs.hashSync(req.body.password, 10);
       }
-    }, {new: true})
-  const {password, ...rest} = updatedUser._doc;
-    res.status(200).json(rest);
-  } catch (error) {
-    next(error);
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        } },
+        { new: true }
+      );
+
+      if (!updatedUser) {
+        return next(errorHandler(404, 'User not found'));
+      }
+
+      const { password, ...rest } = updatedUser._doc;
+      return res.status(200).json(rest);
+    } catch (error) {
+      return next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only update your own account'))
   }
-  res.send(req.body.password)
-}
+};
