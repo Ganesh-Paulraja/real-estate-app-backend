7a95243fbfdb37181db99072acb78f0907461980
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-06-30 20:58:09 +0530

    singout functionality added

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from 'express'
 // import user from '../models/user.model.js';
-import { google, signin, signup } from '../controllers/auth.controller.js';
+import { google, signOut, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signup)
 router.post('/signin', signin)
 router.post('/google', google)
+router.get('/signout', signOut)
 
 // router.get('/signup', (req, res) => {
 //   console.log(User.find());
