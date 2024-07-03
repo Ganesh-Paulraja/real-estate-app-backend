60e7508fac987f5264166ac41a152d69462c83c4
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-03 19:48:58 +0530

    getting user specific listing functionality added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {deleteUser, test, updateUserInfo} from '../controllers/usercontroller.js'
+import {deleteUser, test, updateUserInfo, getUserListing} from '../controllers/usercontroller.js'
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyUser, updateUserInfo);
 router.delete('/delete/:id', verifyUser, deleteUser);
+router.get('/listings/:id', verifyUser, getUserListing);
 
 export default router;
