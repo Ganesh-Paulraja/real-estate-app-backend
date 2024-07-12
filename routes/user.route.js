efc81f07f5687fd0901b548a8799c53020dd344a
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-12 19:36:10 +0530

    developing get user message

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {deleteUser, test, updateUserInfo, getUserListing} from '../controllers/usercontroller.js'
+import {deleteUser, test, updateUserInfo, getUserListing, getUser} from '../controllers/usercontroller.js'
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyUser, updateUserInfo);
 router.delete('/delete/:id', verifyUser, deleteUser);
 router.get('/listings/:id', verifyUser, getUserListing);
+router.get('getuser/:id', getUser)
 
 export default router;
