826db27424c2f45a5698caa3450a4d65ded431e2
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-15 09:27:18 +0530

    search api patch

--- controllers/listing.controller.js
+++ controllers/listing.controller.js
@@ -47,7 +47,7 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
-export const getLising = async (req, res, next) => {
+export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
@@ -58,39 +58,55 @@ export const getLising = async (req, res, next) => {
     next(error)
   }
 }
-export const getLisings = async (req, res, next) => {
+
+export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
-    const seartIndex = parseInt(req.query.startIndex) || 0;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
     let offer = req.query.offer;
     if (offer === undefined || offer === 'false') {
-      offer = {$in: [false, true]};
+      offer = { $in: [false, true] };
+    } else {
+      offer = true;
     }
+
     let furnished = req.query.furnished;
-    if(furnished === undefined || furnished === 'false') {
-      furnished = {$in: [false, true]}
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    } else {
+      furnished = true;
     }
-    let parking = req.query.parking
+
+    let parking = req.query.parking;
     if (parking === undefined || parking === 'false') {
-      parking = {$in: [false, true]}; 
+      parking = { $in: [false, true] };
+    } else {
+      parking = true;
     }
+
     let type = req.query.type;
-    if(type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent']}
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
     }
+
     const searchTerm = req.query.searchTerm || '';
     const sort = req.query.sort || 'createdAt';
-    const order = req.query.order || 'desc'
+    const order = req.query.order === 'asc' ? 1 : -1;
 
-    const listing = await Listing.find({
-      name: {$regex: searchTerm, $options: 'i'},
-      offer, furnished, parking, type,
-    }).sort({[sort]: order})
-    .limit(limit)
-    .skip(seartIndex)
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
 
-    return res.status(200).json(listings)
+    return res.status(200).json(listings);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- routes/listing.route.js
+++ routes/listing.route.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import { verifyUser } from '../utils/verifyUser.js';
-import { createListing, deleteListing, updateListing, getLising, getLisings } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing)
 router.post('/update/:id', verifyUser, updateListing)
-router.get('/get/:id', getLising);
-router.get('/get', getLisings);
+router.get('/get/:id', getListing);
+router.get('/getList', getListings);
 
 export default router;
