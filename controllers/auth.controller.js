851c726e01a059702764c7f05373fe7e4028ea5b
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-29 08:26:59 +0530

    jwt updated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -26,7 +26,7 @@ export const signin = async (req, res, next) => {
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, )
     const {password: pass, ...rest} = validUser._doc;
-    res.cookie('access_token', token, { httpOnly: false })
+    res.cookie('access_token', token, { httpOnly: true })
     .status(200)
     .json(rest)
   } catch (error) {
@@ -41,7 +41,7 @@ export const  google = async (req, res, next) => {
       const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
       const {password: pass, ...rest} = user._doc;
       res
-        .cookie('access_token', token, {httpOnly: false})
+        .cookie('access_token', token, {httpOnly: true})
         .status(200)
         .json(rest);
     } else {
@@ -52,7 +52,7 @@ export const  google = async (req, res, next) => {
       await newUser.save();
       const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET);
       const {password: pass, ...rest} = newUser._doc;
-      res.cookie('access_token', token, {httpOnly: false})
+      res.cookie('access_token', token, {httpOnly: true})
       .status(200)
       .json(rest);
     }
