6dee2756d47bb63a9bfdef474f79f1b455280bcd
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-07-29 07:44:41 +0530

    cors added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 // const cors = require('cors');
-// import cors from 'cors';
+import cors from 'cors';
 dotenv.config();
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
@@ -27,7 +27,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 app.use('/api/user', userRouter);
